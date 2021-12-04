1222c22ccd39af0da32631a75df266c50c6473a3
Two29686 <95551767+Two29686@users.noreply.github.com>
2021-12-04 17:59:47 -0600

    Add files via upload

---
+++ plasma_mod/init.lua
@@ -0,0 +1,32 @@
+minetest.register_node('plasma_mod:red_plasma', {
+    paramtype = 'light',    
+    description = 'red plasma',
+    tiles = {'plasma_texture.png'},
+    group = {snappy=1, oddly_breakable_by_hand=1},
+    light_source = 14, 
+    damage_per_second = 0
+})
+
+minetest.register_craft({
+    output = 'plasma_mod:red_plasma',
+    recipe = {
+        {'default:steelblock','bucket:bucket_lava','default:steelblock'},
+        {'bucket:bucket_lava','bucket:bucket_water','bucket:bucket_lava'},
+        {'default:steelblock','bucket:bucket_lava','default:steelblock'}
+    }
+})
+--minetest.register_node('plasma_mod:red_plasma', {
+	--paramtype = "light",	
+	--description = 'red plasma',
+	--tiles = {'plasma_texture.png'},
+	--group = {snappy=1, oddly_breakable_by_hand=1 }
+	--})light_source = 14, 
+	--damage_per_second = 0,
+--
+--minetest.register_craft({
+	--output = 'plasma_mod:red_plasma'
+	--})recipe = {
+		--{'default:steelblock','bucket:bucket_lava','default:steelblock'}
+		--{'bucket:bucket_lava','bucket:bucket_water','bucket:bucket_lava'}
+		--{'default:steelblock','bucket:bucket_lava','default:steelblock'}
+--})
